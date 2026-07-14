29e61d342a06e3eed60540eaa63719b3fc54cf60
Haksell <axel.brisse@gmail.com>
2026-07-14 12:36:57 +0200

    accuracy on test set

--- mnist.py
+++ mnist.py
@@ -8,8 +8,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset
 
-DEVICE = "cuda"
-
 
 class Classifier(nn.Module):
     def __init__(self):
@@ -54,16 +52,16 @@ class Classifier(nn.Module):
 
 
 class MNIST(Dataset):
-    def __init__(self, csv_file):
+    def __init__(self, csv_file, device):
         # TODO: don't use pandas (and probably not numpy either)
         df = pd.read_csv(csv_file, header=None)
 
         images = df.iloc[:, 1:].to_numpy(dtype="float32", copy=True) / 255
-        self.images = torch.from_numpy(images).to(DEVICE)
+        self.images = torch.from_numpy(images).to(device)
 
         labels = df.iloc[:, 0].to_numpy(dtype="int64", copy=True)
         self.labels = torch.from_numpy(labels)
-        self.targets = F.one_hot(self.labels, 10).to(DEVICE)
+        self.targets = F.one_hot(self.labels, 10).to(device)
 
     def __len__(self):
         return len(self.labels)
@@ -78,7 +76,11 @@ class MNIST(Dataset):
 
 
 def main():
-    assert torch.cuda.is_available()
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+        print("Warning: CUDA is not available.")
 
     PKL_TRAIN = "pkl/mnist_train.pkl"
     PKL_TEST = "pkl/mnist_test.pkl"
@@ -87,40 +89,50 @@ def main():
     try:
         mnist_train = pickle.load(open(PKL_TRAIN, "rb"))
     except FileNotFoundError:
-        mnist_train = MNIST("data/mnist_train.csv")
+        mnist_train = MNIST("data/mnist_train.csv", device)
         pickle.dump(mnist_train, open(PKL_TRAIN, "wb"))
 
     try:
         mnist_test = pickle.load(open(PKL_TEST, "rb"))
     except FileNotFoundError:
-        mnist_test = MNIST("data/mnist_test.csv")
+        mnist_test = MNIST("data/mnist_test.csv", device)
         pickle.dump(mnist_test, open(PKL_TEST, "wb"))
 
     try:
         classifier = pickle.load(open(PKL_CLASSIFIER, "rb"))
     except FileNotFoundError:
-        EPOCHS = 3
-        classifier = Classifier().to(DEVICE)
+        EPOCHS = 1
+        classifier = Classifier().to(device)
         for epoch in range(1, EPOCHS + 1):
             print(f"Training epoch {epoch}/{EPOCHS}")
-            for _, image_data_tensor, target_tensor in mnist_train:
-                classifier.train(image_data_tensor, target_tensor)
+            for _, image_tensor, target_tensor in mnist_train:
+                classifier.train(image_tensor, target_tensor)
         pickle.dump(classifier, open(PKL_CLASSIFIER, "wb"))
 
-    TEST_IDX = 33
-    image_data = mnist_test[TEST_IDX][1]
-    output = classifier.forward(image_data)
+    PLOTS = False
+    if PLOTS:
+        TEST_IDX = 33
+        image = mnist_test[TEST_IDX][1]
+        output = classifier.forward(image)
 
-    pd.DataFrame(output.detach().cpu().numpy()).plot(
-        kind="bar", legend=False, ylim=(0, 1)
-    )
-    plt.show()
+        pd.DataFrame(output.detach().cpu().numpy()).plot(
+            kind="bar", legend=False, ylim=(0, 1)
+        )
+        plt.show()
 
-    mnist_test.plot_image(TEST_IDX)
-    plt.show()
+        mnist_test.plot_image(TEST_IDX)
+        plt.show()
 
-    classifier.plot_progress()
-    plt.show()
+        classifier.plot_progress()
+        plt.show()
+
+    score = items = 0
+    for label, image_tensor, _ in mnist_test:
+        answer = classifier.forward(image_tensor).detach().cpu().numpy().argmax()
+        if answer == label:
+            score += 1
+        items += 1
+    print(score, items, score / items)
 
 
 if __name__ == "__main__":
